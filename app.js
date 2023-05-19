1313fde3471c9a28c9ccc6c7002eeb6e533b4aca
sebas <jesus.huamanculi1@unmsm.edu.pe>
2023-05-18 23:40:59 -0500

    node-cron cada 15 min

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = new Server();
 server.listen();
 
 
-cron.schedule("*/9 * * * * *", ()=>{
+cron.schedule("*/15 * * * * *", ()=>{
     console.log('tarea programada cada 10 segundos');
     Server.tarea();
 });
